70d71d06e88ad247d24933517158c8543c12be7d
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-10-28 14:28:54 -0400

    create routes to Home and Search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Home from './components/containers/Home/Home';
+import Search from './components/containers/Search/Search'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      <p>Hello World</p>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route  path="/search" component={Search} />
+        </Switch>
+      </Router>
     </div>
   );
 }
